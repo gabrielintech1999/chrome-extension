8557f860e618e67c296ebe6a7edae2bd9ee97b56
Gabriel Gomes <gabrielgomesdeveloper1999@gmail.com>
2024-09-05 21:37:46 +0100

    add the first feature of the app

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 console.log("Hello JS");
+
+const textBox = document.getElementById("text-box");
+const itemsContainer = document.getElementById("items-container");
+const saveBtn = document.getElementById("save-btn");
+const tabBtn = document.getElementById("tab-btn");
+const deleteBtn = document.getElementById("delete-btn");
+
+
+const urls = [];
+
+saveBtn.addEventListener("click", addURL)
+
+
+function addURL() {
+    const link = textBox.value
+    textBox.value = ""
+
+    if(link.trim() !== "") {
+        urls.push({
+            link: link
+        })
+
+        render()
+    }
+
+}
+
+
+
+function render(params) {
+ let template = ""
+  urls.forEach((element) => {
+    template += `
+            <li>
+          <a href=${element.link}>${element.link}</a>
+         </li>
+        `;
+  });
+
+  itemsContainer.innerHTML = template
+}
