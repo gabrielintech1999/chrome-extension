9dbb6c753f9c761095b71a5a8c8fc06861527dcd
Gabriel Gomes <gabrielgomesdeveloper1999@gmail.com>
2024-09-10 18:07:23 +0100

    make tab to fuction...

--- index.js
+++ index.js
@@ -6,10 +6,35 @@ const saveBtn = document.getElementById("save-btn");
 const tabBtn = document.getElementById("tab-btn");
 const deleteBtn = document.getElementById("delete-btn");
 
+console.log(tabBtn);
 
-const urls = [];
+
+let urls = [];
 
 saveBtn.addEventListener("click", addURL)
+deleteBtn.addEventListener("click", deleteURL)
+
+
+function deleteURL() {
+  urls = []
+  render()
+}
+
+tabBtn.addEventListener("click", function(){
+  console.log("Ok");
+  
+ /* chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      // since only one tab should be active and in the current window at once
+      // the return variable should only have one entry
+      let activeTab = tabs[0]
+      let activeTabId = activeTab.id // or do whatever you need
+  })
+  
+  urls.push(tabs[0].url)*/
+
+  
+})
+
 
 
 function addURL() {
